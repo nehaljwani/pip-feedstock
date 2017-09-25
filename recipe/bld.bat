56498da3a241bb4973b366fcc8da3fb5bea6f7a7
Michael Sarahan <msarahan@gmail.com>
2017-09-25 12:50:31 -0500

    unbundle vendored deps due to path length issues on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,10 @@
+:: remove all the things that pip vendors - use our own
+::    this is only important on windows, where we bump into path length limits.
+pushd pip\_vendor
+rd /s /q cachecontrol colorama distlib html5lib packaging lockfile pkg_resources progress requests webencodings
+del six.py
+popd
+
 python setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
 
